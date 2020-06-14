ac81dd3d370cfad7c0dcee4fe5c034126eb10f77
DmitriyRminiaka <reminyakad23@gmail.com>
2020-06-14 18:27:44 +0300

    homework_5

---
+++ homework_5/js/task-5.js
@@ -0,0 +1,76 @@
+class Car {
+  static getSpecs(car) {
+    console.log(
+      `maxspeed: ${car.maxSpeed}, speed: ${car.speed}, isOn: ${car.isOn}, distance: ${car.distance}, price: ${car.price}`
+    );
+  }
+
+  constructor({ price, maxSpeed }) {
+    this.speed = 0;
+    this._price = price;
+    this.maxSpeed = maxSpeed;
+    this.isOn = false;
+    this.distance = 0;
+  }
+
+  get price() {
+    return this._price;
+  }
+
+  set price(cost) {
+    return (this._price = cost);
+  }
+
+  turnOn() {
+    this.isOn = true;
+  }
+
+  turnOff() {
+    this.isOn = false;
+    this.speed = 0;
+  }
+
+  accelerate(value) {
+    if (this.speed + value > this.maxSpeed) {
+      this.speed = this.maxSpeed;
+    } else {
+      this.speed += value;
+    }
+  }
+
+  decelerate(value) {
+    if (this.speed - value < 0) {
+      this.speed = 0;
+    } else {
+      this.speed -= value;
+    }
+  }
+
+  drive(hours) {
+    if (this.isOn) {
+      this.distance += this.speed * hours;
+    }
+  }
+}
+const mustang = new Car({
+  maxSpeed: 200,
+  price: 2000,
+});
+
+mustang.turnOn();
+mustang.accelerate(50);
+mustang.drive(2);
+
+Car.getSpecs(mustang);
+// maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
+
+mustang.decelerate(20);
+mustang.drive(1);
+mustang.turnOff();
+
+Car.getSpecs(mustang);
+// maxSpeed: 200, speed: 30, isOn: false, distance: 130, price: 2000
+
+console.log(mustang.price); // 2000
+mustang.price = 4000;
+console.log(mustang.price); // 4000
